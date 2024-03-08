abbe81a3a841224305a875c52253f98b7046981f
adhikariprayag <adhikariprayag1@gmail.com>
2024-03-08 21:28:28 +0545

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,100 @@
-import Image from "next/image";
+"use client"
+import React, { useRef, useState } from 'react';
+import 'remixicon/fonts/remixicon.css'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination, EffectFade, Autoplay } from 'swiper/modules';
+import Image from 'next/image';
+import 'swiper/css/effect-fade';
+
+//Importing Backgrounds
+import Bg1 from '../img/bg1.jpg'
+import Bg2 from '../img/bg2.jpg'
+import Bg3 from '../img/bg3.jpg'
+import Bg4 from '../img/bg4.jpg'
+import b1 from '../img/13.jpg'
+import b2 from '../img/12.jpg'
+import b3 from '../img/11.jpg'
+import b4 from '../img/10.jpg'
+
+import './home.scss'
+import 'animate.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+
+    <main>
+      <div className="slider relative">
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          effect={'fade'}
+          modules={[Pagination, EffectFade, Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
+              <Image src={Bg1} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
+              <p className=' text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>HYDRO</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-sky-500'>.</span></h5>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
+              <Image src={Bg2} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
+              <p className=' text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>SOLAR</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-yellow-400'>.</span></h5>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
+              <Image src={Bg3} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
+              <p className=' text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>BIOGAS</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-slate-400'>.</span></h5>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
+              <Image src={Bg4} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
+              <p className=' text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>WIND</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-slate-400'>.</span></h5>
+            </div>
+          </SwiperSlide>
+
+        </Swiper>
+
+        <div className='md:absolute block right-0 bottom-0 video-box w-full md:w-[530px] '>
+          <a href="https://www.youtube.com/watch?v=1kUE0BZtTRc" className='zoom-in' target='_blank'>
+            <div className="video-con after:content-['&#xF00B'] ">
+              <Image src={Bg3} width={0} height={0} alt='video' className='h-full w-full'></Image>
+            </div>
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="boxes flex flex-wrap">
+        <div className='box box-1 lg:w-1/4 md:w-1/2 w-full'>
+          <Image src={b1} width={0} height={0} alt='background' className='h-[300px] object-cover'></Image>
+        </div>
+        <div className='box box-4 lg:w-1/4 md:w-1/2 w-full'>
+          <Image src={b2} width={0} height={0} alt='background' className='h-[300px] object-cover'></Image>
+        </div>
+        <div className='box box-3 lg:w-1/4 md:w-1/2 w-full'>
+          <Image src={b3} width={0} height={0} alt='background' className='h-[300px] object-cover'></Image>
+        </div>
+        <div className='box box-4 lg:w-1/4 md:w-1/2 w-full'>
+          <Image src={b4} width={0} height={0} alt='background' className='h-[300px] object-cover'></Image>
+        </div>
+      </section>
     </main>
   );
 }
---
+++ components/Navbar.js
@@ -0,0 +1,96 @@
+"use client"
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import 'remixicon/fonts/remixicon.css'
+import './/Navbar.scss'
+import Logo from '../img/logo.png'
+import Link from 'next/link'
+
+function Navbar() {
+
+    const [showSidebar, setShowSidebar] = useState(true)
+    function handleClick() {
+        setShowSidebar(showSidebar => !showSidebar)
+    }
+
+    return (
+        <>
+
+            {/* TOP NAVBAR */}
+            <header className='navbar'>
+                <section className='navbar-top py-2 bg-[#f9f9f9] text-sm text-[#8f8f8f] hidden lg:block'>
+                    <div className="container mx-auto flex flex-wrap justify-between px-5">
+
+                        <div className='flex flex-wrap gap-3'>
+                            <div><p className='m-0 p-0 uppercase font-medium text-black'>Follow Us:</p></div>
+                            <ul className='p-0 m-0 flex flex-wrap gap-3 text-black'>
+                                <li><i className="ri-facebook-line"></i></li>
+                                <li><i className="ri-twitter-x-line"></i></li>
+                                <li><i className="ri-instagram-line"></i></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul className='p-0 m-0 flex flex-wrap gap-7 '>
+                                <li><i className="ri-time-line"></i> Mon-Sat 8:00 - 18:00</li>
+                                <li><i className="ri-smartphone-line"></i> 0800 123 4567</li>
+                                <li><i className="ri-send-plane-fill"></i> eco@example.com</li>
+                                <li><i className="ri-map-pin-line"></i> 7 Oakmound Drive, Chicago, IL 60606</li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </header>
+            {/* NAVBAR WITH SCROLL ANIMATION */}
+            <section className="navbar-main py-6 px-5 bg-[#ffffffee] sticky top-0 shadow">
+                <div className="container mx-auto flex flex-wrap items-center justify-between">
+                    <div>
+                        <Link href="/"><Image priority src={Logo} alt='logo' width={0} height={0} className='object-cover'></Image></Link>
+                    </div>
+
+                    <div className='nav-item-mid hidden lg:block'>
+                        <ul className='flex flex-wrap gap-14 p-0 m-0 uppercase text-sm font-semibold text-black'>
+                            <li><Link href="/" className='p-0 m-0'>Home</Link></li>
+                            <li><Link href="/about" className='p-0 m-0'>About</Link></li>
+                            <li><Link href="/" className='p-0 m-0'>Services</Link></li>
+                            <li><Link href="/" className='p-0 m-0'>Blog</Link></li>
+                            <li><Link href="/" className='p-0 m-0'>Contact</Link></li>
+                        </ul>
+                    </div>
+
+                    <div className='nav-item-btn flex items-center gap-5 flex-wrap'>
+                        <div className='hidden sm:block'>
+                            <Link href="/contact" className='uppercase py-4 px-6 bg-[#27D2A3] font-medium '>Request A Quote</Link>
+                        </div>
+                        <div className='block lg:hidden'>
+                            <i className="ri-menu-2-line text-3xl cursor-pointer" onClick={handleClick}></i>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+            <div className={showSidebar ? 'hidden' : 'block '}>
+                <div className="sidebar flex flex-col justify-start ps-5 pt-10 items-center md:items-start text-center md:text-start h-[100dvh] md:w-[350px] w-[100%] shadow-black shadow-sm bg-[#ffffff36] fixed top-0 right-0 z-[99999] backdrop-blur-md ">
+
+                    <div className='flex justify-between w-[100%] items-center px-3'>
+                        <Link href="/"><Image src={Logo} width={0} height={0} alt='logo' className='w-[120px]'></Image></Link>
+                        <i className="ri-close-line text-3xl cursor-pointer" onClick={handleClick}></i>
+                    </div>
+                    <ul className='p-0 m-0 flex flex-col gap-7 mt-[50px] uppercase text-xl font-semibold text-black'>
+                        <li><Link href="/" className='p-0 m-0'>Home</Link></li>
+                        <li><Link href="/about" className='p-0 m-0'>About</Link></li>
+                        <li><Link href="/" className='p-0 m-0'>Services</Link></li>
+                        <li><Link href="/" className='p-0 m-0'>Blog</Link></li>
+                        <li><Link href="/" className='p-0 m-0'>Contact</Link></li>
+                    </ul>
+
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+
+
+export default Navbar
