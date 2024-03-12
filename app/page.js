706c16d3d8da25fa194ba52d8b1a0bef1d7758c8
adhikariprayag <adhikariprayag1@gmail.com>
2024-03-12 21:47:21 +0545

    edits

--- app/page.js
+++ app/page.js
@@ -20,9 +20,11 @@ import b1 from '../img/13.jpg'
 import b2 from '../img/12.jpg'
 import b3 from '../img/11.jpg'
 import b4 from '../img/10.jpg'
+import lf from '../img/34.jpg'
 
 import './home.scss'
 import 'animate.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -45,29 +47,29 @@ export default function Home() {
           <SwiperSlide>
             <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
               <Image src={Bg1} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
-              <p className='text-[80px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>HYDRO</p>
-              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-sky-500'>.</span></h5>
+              <p className='text-[54px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>HYDRO</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[54px] md:text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-sky-500'>.</span></h5>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
               <Image src={Bg2} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
-              <p className=' text-[80px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>SOLAR</p>
-              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-yellow-400'>.</span></h5>
+              <p className='text-[54px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>SOLAR</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[54px] md:text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-yellow-400'>.</span></h5>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
               <Image src={Bg3} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
-              <p className=' text-[80px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>BIOGAS</p>
-              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-slate-400'>.</span></h5>
+              <p className='text-[54px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>BIOGAS</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[54px] md:text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-slate-400'>.</span></h5>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className='img-wrapper w-full h-[100dvh] relative flex justify-center items-center text-wrap'>
               <Image src={Bg4} width={0} height={0} alt='background' className='object-cover h-full w-full'></Image>
-              <p className=' text-[80px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>WIND</p>
-              <h5 className='animate__animated animate__fadeInRight absolute text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-slate-400'>.</span></h5>
+              <p className='text-[54px]  md:text-[120px] font-extrabold absolute text-stroke animate__animated animate__fadeInRight'>WIND</p>
+              <h5 className='animate__animated animate__fadeInRight absolute text-[54px] md:text-[80px] font-extrabold mt-[180px] text-light text-white'>energy <span className='text-slate-400'>.</span></h5>
             </div>
           </SwiperSlide>
 
@@ -81,8 +83,9 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <section className="boxes flex flex-wrap">
 
+      {/* CARDS */}
+      <section className="boxes flex flex-wrap">
 
         <div className='box box-1 lg:w-1/4 md:w-1/2 w-full h-[320px] flex justify-center items-center flex-col relative'>
           <Image src={b1} width={0} height={0} alt='background' className='h-[320px] w-full object-cover '></Image>
@@ -110,6 +113,23 @@ export default function Home() {
           <i className="ri-bar-chart-horizontal-line text-[54px] absolute top-[30%] text-white"></i>
           <p className='text-[54px] font-extrabold absolute top-[50%] text-stroke'>WIND</p>
           <h5 className='text-[34px] font-extrabold text-white absolute top-[68%]'>energy <span className='text-green-500'>.</span></h5>
+        </div>
+      </section>
+
+      <section className='py-11 info-section'>
+        <div className="container mx-auto p-8 flex lg:flex-row flex-col  justify-center gap-8 lg:justify-between items-center max-w-[1400px]">
+          <div className="lg:w-[60%] lg:p-20 md w-full">
+            <div className='w-full h-full img-turbine'>
+              <Image src={lf} width={0} height={0} alt='bg'></Image>
+            </div>
+          </div>
+          <div className='lg:w-[40%] w-full'>
+            <h2 className='font-bold text-[24px] md:text-6xl w-[80%] text-[#2D3545] text-wrap'>Investing in clean energy generation</h2>
+            <p className='text-gray-600 py-12 w-[70%] leading-8 font-medium'>We’re among the world’s leading renewable energy solutions providers that is revolutionising and redefining the way sustainable energy sources are harnessed across the world. We are presented in many countries across the world.</p>
+            <div className='nav-item-btn '>
+              <Link href="/contact" className='uppercase md:py-4 md:px-6 py-4 px-2 bg-[white border-[1px] border-black font-medium '>MORE ABOUT US</Link>
+            </div>
+          </div>
         </div>
       </section>
     </main>
